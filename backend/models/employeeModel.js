aa58d32937acc65d03168bc1d496c28827a4c331
AlinaZeif <77826451+AlinaZeif@users.noreply.github.com>
2021-04-11 13:44:54 +0300

    Update employeeModel.js

--- backend/models/employeeModel.js
+++ backend/models/employeeModel.js
@@ -3,12 +3,12 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 export const EmployeeSchema= new Schema({
-    firstName: { type: String, required: true },
+    image: { type: String, required: true },
+	firstName: { type: String,  required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true },
     phone: { type: Number },
     isCoach: {type: Boolean, default: false},
-    _id: {  type: String, required: true },
     team: { type: String },
     created_date: {type: Date, default: Date.now}
 });
